36e9209d7e28a718d89b3a3e72a215add264c357
Cecilia Marquez <cmarque1@gmail.com>
2012-11-12 10:19:42 -0500

    ruby ex5

---
+++ ex5.rb
@@ -0,0 +1,24 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about %s." % my_name
+puts "He's %d inches tall." % my_height
+puts "He's %d pounds heavy." % my_weight
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+puts "If I add %d, %d, and %d I get %d." % [
+    my_age, my_height, my_weight, my_age + my_height + my_weight]
+
+ # %s--string %d--integer
+ #the advantage to this is that you could make a function and have it change everywhere that there is my_name
+
+puts "He's %d centimeters tall." % [my_height/2.54]
+#use %f it uses floating point
