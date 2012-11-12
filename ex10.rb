7c9ae61dd9daf8c44402324a265d3a8724962f49
Cecilia Marquez <cmarque1@gmail.com>
2012-11-12 11:20:31 -0500

    ruby ex10

---
+++ ex10.rb
@@ -0,0 +1,21 @@
+#don't forget you need "puts" to make things show up on terminal
+puts "I am 6'2\" tall."  # escape double-quote inside string
+puts 'I am 6\'2" tall.'  # escape single-quote inside string
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+# backslash is a special character \n--new line \t--tab
