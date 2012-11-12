289bb58023dbc8ff52f11ab4cac74a36eb7ac884
Cecilia Marquez <cmarque1@gmail.com>
2012-11-12 08:59:30 -0500

    ruby ex1

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,7 @@
 puts "Hello World!"
-a = 2+4
-puts a
+puts "Hello Again"
+puts "I like typing this."
+puts #"This is fun."
+puts 'Yay! Printing'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
